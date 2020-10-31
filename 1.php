ac85ca91451ac5c3597e6f8aa4a427a03dc2adb3
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2020-10-31 20:22:32 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,16 +1,21 @@
 <?php
 function tugas1($var){
-	$spl = explode(" ", $var);
-	$jum = count($spl);
-	for($i = 1; $i <= $jum; $i++){
-		for ($u = 0; $u < $jum; $u++) { 
-			$x = preg_replace("/[^0-9]/", "", $spl[$u]);
-			if($x == $i){
-				$res[] = $spl[$u];
+	if(empty($var)){
+		$res = "Data Kosong Bang";
+	}else{
+		$spl = explode(" ", $var);
+		$jum = count($spl);
+		for($i = 1; $i <= $jum; $i++){
+			for ($u = 0; $u < $jum; $u++) { 
+				$x = preg_replace("/[^0-9]/", "", $spl[$u]);
+				if($x == $i){
+					$res[] = $spl[$u];
+				}
 			}
 		}
+		$res = join(" ", $res);
 	}
-	return join(" ", $res);
+	return $res;
 }
 
 $str = "ming3gu k7ota Pa1da a5yah ha2ri k6e kutu4rut";
