c101345c6504b3407a9207c28038c79ba43a97f1
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2020-11-01 03:58:41 +0800

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,16 +1,14 @@
 <?php
-function tugas2($var){
-	$jm = count($var);
-	$a = "";
-	for($i = 0; $i < $jm; $i++){
-		if($i > 0){
-			$a = str_repeat("= ",$i);
-		}
-		$res[] = $a . $var[$i];
-	}
-	return join("\n",$res);
+function tugas2($str){
+  for($i=1;$i<=count($str)/2;$i++){
+    $ck[] = str_repeat("= ",$i-1).$str[$i-1].str_repeat(" =",count($str)-$i*2)." ".$str[$i-1].str_repeat(" =",$i-1);
+
+    $x=count($str)/2+$i;
+    $ck2[] = str_repeat("= ",count($str)-$x).$str[$x-1].str_repeat(" =",($x-count($str)/2-1)*2)." ".$str[$x-1].str_repeat(" =",count($str)-$x);
+  }
+  return join("\n",$ck)."\n".join("\n",$ck2);
 }
 
 $string = ['D','U','M','B','W','A','Y','S','I','D'];
-echo tugas2($string);
+print_r(tugas2($string));
 ?>
